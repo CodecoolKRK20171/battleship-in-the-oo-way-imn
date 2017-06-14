501dad689b62300841a30a9b0c439faeb0f5cc18
natahrytsyna <natalya.hrytsyna@gmail.com>
2017-06-14 12:55:35 +0200

    added inputs as parameters in functions

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from player import Player
 
 
 
+# BATTLESHIP_SIZES = [['Carrier', 5],
+#                     ['Battleship', 4],
+#                     ['Cruiser', 3],
+#                     ['Submarine', 3],
+#                     ['Destroyer', 2]]
+
 BATTLESHIP_SIZES = [['Carrier', 5],
-                    ['Battleship', 4],
-                    ['Cruiser', 3],
-                    ['Submarine', 3],
-                    ['Destroyer', 2]]
+                    ['Battleship', 4]]
 
 
 def print_table(table, title_list):
@@ -66,36 +69,95 @@ def main():
     ocean1 = Ocean()
     ocean1.load_board()
     player = Player(player1, ocean1)
-    print_table(BATTLESHIP_SIZES, ['Battleship', 'Size'])
 
 
-    for ship in BATTLESHIP_SIZES:
-        ship_choice = input('Enter which ship you would like to locate:')
-        if ship_choice.upper() == ship[0].upper():
-            ship_type = ship
-            BATTLESHIP_SIZES.remove(ship)
-    print(BATTLESHIP_SIZES)
+    # for ship in BATTLESHIP_SIZES:
+    #     print_table(BATTLESHIP_SIZES, ['Battleship', 'Size'])
+    #     ship_choice = input('Enter which ship you would like to locate:')
+    #     if ship_choice.upper() == ship[0].upper():
+    #         ship_size = ship[1]
+    #         BATTLESHIP_SIZES.remove(ship)
+    #         print(BATTLESHIP_SIZES)
+    #
+    #         is_horizontal = int(input('\nEnter if your ship is horizontal(1/0):'))
+    #
+    #         print('\nNow you need to locate your battleships')
+    #         position = input('Enter coordinates(e.g. H5):')
+    #         letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    #
+    #         for letter in  letters:
+    #             if position[0].upper() == letter:
+    #                 position_x = letters.index(letter)
+    #         position_y = position[1]
+    #
+    #     else:
+    #         print('There is no such ship!')
+
+
+
+    ship_kinds = []
+    for i in range(len(BATTLESHIP_SIZES)):
+        ship_kinds.append(BATTLESHIP_SIZES[i][0])
+    print(ship_kinds)
+
+    while len(BATTLESHIP_SIZES) > 0:
+        print_table(BATTLESHIP_SIZES, ['Battleship kind', 'Size'])
+        ship_choice = input('Enter which ship you would like to locate: ')
+        if ship_choice in ship_kinds:
+            ship_size = int(BATTLESHIP_SIZES[i][1])
+            print(ship_size)
+
+            index_to_del = 0
+            for i in range(len(BATTLESHIP_SIZES)):
+                if ship_choice in BATTLESHIP_SIZES[i]:
+                    break
+                else:
+                    index_to_del += 1
+
+            del BATTLESHIP_SIZES[index_to_del]
+            ship_kinds.remove(ship_choice)
+            print(BATTLESHIP_SIZES)
+
+            is_horizontal = int(input('\nEnter if your ship is horizontal(1/0): '))
+
+            print('\nNow you need to locate your battleships')
+            position = input('Enter coordinates(e.g. H5): ')
+            letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+            for letter in  letters:
+                if position[0].upper() == letter:
+                    position_x = int(letters.index(letter))
+            position_y = int(position[1])
+
+            player.add_ship(position_x, position_y, ship_size, True)
+
+            for row in ocean1.board:
+                for column in row:
+                    column.un_hide()
+            print(ocean1)
+        else:
+            print('There is no such ship!')
+
+    print('statki wybrane')
+
+
+
         # else:
         #     raise IndexError('There are only 5 ships!')
 
 
 
-    is_horizontal = int(input('\nEnter if your ship is horizontal(1/0):'))
-    print('\nNow you need to locate your battleships')
-    position_x = int(input('\nEnter X coordinate:'))
-    position_y = int(input('\nEnter Y coordinate:'))
 
 
 
 
 
 
-    player.add_ship(1, 5, 3, True)
     player.add_ship(4, 4, 3, False)
     player.shot(4, 4)
     player.shot(1, 5)
     player.shot(5, 1)
-    print(ocean1)
+
 
 
 if __name__ == '__main__':
